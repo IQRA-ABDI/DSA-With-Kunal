e1a507a383044bad3ba9ba3166179d1a8fe7d7ce
IQRA-ABDI <yourname@domain.com>
2022-11-10 23:27:03 +0300

    Example two

---
+++ All-Examples/example2.java
@@ -0,0 +1,74 @@
+// if you want to take the comments of the program,you can use (ctrl+/)
+
+// import java.util.Scanner;
+// import java.util.ArrayList;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         //--switch statements
+//         Scanner in = new Scanner(System.in);
+//         System.out.println("enter a fruit");
+//         String fruit= in.next();
+//         switch (fruit){
+//             case "mango":
+//                 System.out.println("king of fruits");
+//                 break;
+//             case "apple":
+//                 System.out.println("a sweet red fruit");
+//                 break;
+//             case "orange":
+//                 System.out.println("round fruit");
+//                 break;
+//             case "grapes":
+//                 System.out.println("smell fruit");
+//                 break;
+//             default:
+//                 System.out.println("please enter a valid fruit");
+//         }
+//        // --another way you can use switch statements
+//         System.out.println("enter the day");
+//         int day=in.nextInt();
+//         switch (day){
+//             case 1 -> System.out.println("Saturday");
+//             case 2 -> System.out.println("Sunday");
+//             case 3 -> System.out.println("Monday");
+//             case 4 -> System.out.println("Tuesday");
+//             case 5 -> System.out.println("Wednesday");
+//             case 6 -> System.out.println("Thursday");
+//             case 7 -> System.out.println("Friday");
+//         }
+
+
+//         //--function /methods
+//         main();//this is function call
+//         main();
+//     }
+//     static void main(){
+
+//         Scanner in =new Scanner(System.in);
+//         System.out.println("enter number1");
+//         int num1= in.nextInt();
+//         System.out.println("enter number2");
+//         int num2= in.nextInt();
+//         int sum=num1+num2;
+//         System.out.println("the sum is = "+sum);
+
+//          //--array example
+//         int[]arr1={2,3,4,5,6,7,8};
+//         System.out.println(arr1[6]);
+
+//         //--array length
+//         int[]arr1={2,3,4,5,6,7,8};
+//        System.out.println(arr1.length);
+//      // --Arraylist
+//         ArrayList<String> cars = new ArrayList<String>();
+//         cars.add("Volvo");
+//         cars.add("BMW");
+//         cars.add("Ford");
+//         cars.add("Mazda");
+//         cars.remove(0);
+//         cars.size();
+//         System.out.println(cars);
+//     }
+
+// }
