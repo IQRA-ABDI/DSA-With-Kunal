d7fd654658f544540d3ba7a9172c459eb44b53ed
IQRA-ABDI <yourname@domain.com>
2022-11-11 18:58:18 +0300

    Solved

--- Assignments/Assign1.java
+++ Assignments/Assign1.java
@@ -1,4 +1,4 @@
-Question 1
+--Question 1
 start
 //input year
 year =
@@ -6,7 +6,7 @@ if year %4||100 =0
 output leap year
 end
 
-Question 2
+--Question 2
 start
 //input num1
 num1=
@@ -16,19 +16,19 @@ sum=num1+num2
 output sum
 end
 
-Question 3
+--Question 3
 start
-num1=sc.nextInt()
+num1=scanner.nextInt()
 for 1 to 10 i++
 multiplication_table=inum1
 printf=%d %d=%d,num1,i,multiplication_table//should be inside the loop
-sample imput 2
+sample input 2
 output 21=2,22=4,23=6,24=8,25=10,26=12,27=14,28=16,29=18,2*10=20
 end
 
-Question 4
+--Question 4
 start
-num1=sc.nextInt()
+num1=sc	.nextInt()
 num2=sc.nextInt()
 temp1=num1
 temp2=num2
@@ -44,9 +44,9 @@ sample input num1=12 num2=6
 output hcf=6 lcm=12
 end
 
-Question 5
-t=sc.nextInt();
-while(sc.hasNextInt())
+--Question 5
+t=scanner.nextInt();
+while(scanner.hasNextInt())
 //inside the loop
 num=sc.nextint();
 for j=0 j<=i i++
