ff734427defaa320d4f49397e516f87f7c793161
IQRA-ABDI <yourname@domain.com>
2022-12-17 19:38:55 +0300

    added

--- Assignments/04-Functions/Function-Solutions.java
+++ Assignments/04-Functions/Function-Solutions.java
@@ -177,7 +177,7 @@ static int min(int a , int b ,int c) {
                return(n * factorial(n-1));
        }
        public static void main(String args[]){
-           int i,fact=1;
+           int fact=1;
            int number=4;//It is the number to calculate factorial
            fact = factorial(number);
            System.out.println("Factorial of "+number+" is: "+fact);
